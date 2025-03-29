6237e97667a49d3a0c28c1bb0569fd7f0b4a10ce
Swarnalatha <155281967+swarnalatha-coder@users.noreply.github.com>
2025-03-29 11:11:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            Solution ob = new Solution();
+            int[] ans = new int[n];
+            ans = ob.productExceptSelf(arr);
+
+            for (int i = 0; i < n; i++) {
+                System.out.print(ans[i] + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public static int[] productExceptSelf(int arr[]) {
+        int n=arr.length;
+        int[] res=new int[n];
+        int leftProduct=1;
+        for(int i=0;i<n;i++){
+            res[i]=leftProduct;
+            leftProduct*=arr[i];
+        }
+        int rightProduct=1;
+        for(int i=n-1;i>=0;i--){
+            res[i]*=rightProduct;
+            rightProduct*=arr[i];
+        }
+    return res;
+    }
+}
