8eddbe2da555d356578d72f1495bf6f3dad50efb
Swarnalatha <155281967+swarnalatha-coder@users.noreply.github.com>
2025-04-01 14:27:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+            int V = Integer.parseInt(br.readLine().trim());
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+            for (int i = 0; i < V; i++) {
+                String[] input = br.readLine().trim().split(" ");
+                ArrayList<Integer> node = new ArrayList<>();
+                for (String s : input) {
+                    if (!s.isEmpty()) {
+                        node.add(Integer.parseInt(s));
+                    }
+                }
+                adj.add(node);
+            }
+
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfs(adj);
+            for (int num : ans) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean[] visited = new boolean[adj.size()];
+        ArrayList<Integer> res = new ArrayList<>();
+        dfsRec(adj, visited, 0, res);
+        return res;
+    }
+
+    // To add an edge in an undirected graph
+    public static void
+    addEdge(ArrayList<ArrayList<Integer> > adj, int s,
+            int t)
+    {
+        adj.get(s).add(t);
+        adj.get(t).add(s);
+    }
+     private static void
+    dfsRec(ArrayList<ArrayList<Integer> > adj,
+           boolean[] visited, int s, ArrayList<Integer> res)
+    {
+        visited[s] = true;
+        res.add(s);
+
+        // Recursively visit all adjacent vertices that are
+        // not visited yet
+        for (int i : adj.get(s)) {
+            if (!visited[i]) {
+                dfsRec(adj, visited, i, res);
+            }
+        }
+    }
+}
