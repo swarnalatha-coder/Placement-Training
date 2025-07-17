ed09d82fd74e09dc7d17dc17a0c06d0e0f2ecff6
Swarnalatha Malkani <155281967+swarnalatha-coder@users.noreply.github.com>
2025-07-17 14:53:50 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (83.88%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int count=numBottles;
+        while(numBottles>=numExchange){
+            count+=numBottles/numExchange;
+            numBottles=numBottles/numExchange+numBottles%numExchange;
+        }
+        return count;
+    }
+}
