64e88ff8d8fb294bd6ca1bfed3d9af06e0ddd221
Swarnalatha Malkani <155281967+swarnalatha-coder@users.noreply.github.com>
2025-08-08 14:51:51 +0530

    Time: 10 ms (54.03%) | Memory: 45.6 MB (9.63%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character,Integer> dict = new HashMap<>();
+        for(char ch : magazine.toCharArray()){
+            dict.put(ch,dict.getOrDefault(ch,0)+1);
+        }
+        for(char c:ransomNote.toCharArray()){
+            if(!dict.containsKey(c)){
+                return false;
+            }
+            else if(dict.get(c)==0){
+                return false;
+            }
+            else{
+                dict.put(c,dict.get(c)-1);
+            }
+        }
+        return true;
+   }
+}
