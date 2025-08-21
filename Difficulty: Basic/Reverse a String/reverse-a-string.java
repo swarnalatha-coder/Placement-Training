44a368420c0a3f708ffdfbea152267128f221f2a
Swarnalatha Malkani <155281967+swarnalatha-coder@users.noreply.github.com>
2025-08-21 15:31:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.java
+++ Difficulty: Basic/Reverse a String/reverse-a-string.java
@@ -1,30 +1,12 @@
-//{ Driver Code Starts
-// Initial template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String s = read.readLine();
-            Solution ob = new Solution();
-
-            System.out.println(ob.revStr(s));
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
-
+// User function Template for Java
 
 class Solution {
-    static String revStr(String s) {
-          String reversed = new StringBuilder(s).reverse().toString();
-            return reversed;
+    public static String reverseString(String s) {
+        // code here
+        String reversed="";
+        for(int i=s.length()-1;i>=0;i--){
+            reversed+=s.charAt(i);
+        }
+        return reversed;
     }
-  
 }
