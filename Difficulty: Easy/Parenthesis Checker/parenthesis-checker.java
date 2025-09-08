880d9c82c5ab08346314a2b5a3ef7855a953dec2
Swarnalatha Malkani <155281967+swarnalatha-coder@users.noreply.github.com>
2025-09-08 10:24:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -1,70 +1,27 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Driverclass {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-
-        // Reading total number of testcases
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            // reading the string
-            String st = sc.next();
-
-            // calling ispar method of Paranthesis class
-            // and printing "balanced" if it returns true
-            // else printing "not balanced"
-            if (new Solution().isBalanced(st) == true)
-                System.out.println("true");
-            else
-                System.out.println("false");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-
 class Solution {
-    static boolean isBalanced(String s) {
+    public boolean isBalanced(String s) {
         // code here
-        if(s.length()%2!=0){
-			return false;
-		}
-		Stack<Character> stack=new Stack<>();
-		for(int i=0;i<s.length();i++){
-			if(s.charAt(i)=='[' || s.charAt(i)=='{' || s.charAt(i)=='('){
-				stack.push(s.charAt(i));
-			}
-			else if(stack.empty()){
-				return false;
-			}
-			else{
-				if(s.charAt(i)==')'){
-					if(stack.pop()!='('){
-						return false;
-					}
-				}
-				if(s.charAt(i)=='}'){
-					if(stack.pop()!='{'){
-						return false;
-					}
-				}
-				if(s.charAt(i)==']'){
-					if(stack.pop()!='['){
-						return false;
-					}
-				}
-			}
-		}
-		if(stack.empty()){
-			return true;
-		}
-		return false;
+        Stack<Character> st = new Stack<>();
+        for(char ch: s.toCharArray()){
+            if(ch=='(' || ch== '[' || ch== '{'){
+                st.push(ch);
+            }
+            else if(ch==')'){
+                if(st.isEmpty() || st.pop()!='('){
+                   return false;
+                }
+            }
+            else if(ch==']'){
+                if(st.isEmpty() || st.pop()!='['){
+                  return false;
+                }
+            }
+            else if(ch=='}'){
+                if(st.isEmpty() || st.pop()!='{'){
+                 return false;
+                }
+            }
+        }
+        return st.isEmpty();
     }
 }
